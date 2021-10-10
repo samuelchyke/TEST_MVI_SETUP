3cf64215f1f4e6180dd75c856032e5086a65760e
samuel chyke <samuelchyke@hotmail.com>
2021-10-10 16:30:27 +0100

    Setup

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+
+        button_calculate.setOnClickListener {
+            calculate()
+        }
+
+
     }
+
+    private fun calculate(){
+        val a = text_view_answer_1.toString().toInt()
+        val b = text_view_answer_2.toString().toInt()
+
+        val result = a + b
+
+        textview.setText(result)
+    }
+
+
 }
